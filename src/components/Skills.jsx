16e3c16fcdb461e8be52a89349326c85a1175a99
rohitrodge <rohitrodge27@gmail.com>
2023-08-29 19:30:31 +0530

    add babel file in package.json filr

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,7 +14,7 @@ import sql from '../assests/projects/sql.png';
 const Skills = () => {
   return (
     <div name ='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
-     {/* container */}
+
        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
              <div>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
