bc5bf3a124fc74df4bd982c55181bb70177ae14e
rohitrodge <rohitrodge27@gmail.com>
2023-08-31 16:08:47 +0530

    color changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
 
+
 const Home = () => {
+
   return (
     <div name="home" className='w-full h-screen bg-[#0a192f]'>
 
